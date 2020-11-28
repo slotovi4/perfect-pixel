a860c32dd48f8efc3cfee9e64262943288a3f18d
‘Роман <roman_bruns@elon.team>
2020-11-28 16:28:31 +0300

    feat: add transparent window

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,24 +4,50 @@ import logo from './logo.svg';
 import './Home.scss';
 
 const Home = ({ text, onHeaderClick }: IProps) => {
-    const home = cn('Home');
+	const home = cn('Home');
+	const [imageUrl, setImageUrl] = React.useState<string | null>(null);
 
-    return (
-        <section className={home()}>
-            <header className={home('Header')} onClick={onHeaderClick}>
-                <img src={logo} className={home('Logo')} alt="logo" />
-                <h1 className={home('Title')}>{text || 'Welcome to React'}</h1>
-            </header>
-            <p className={home('Intro')}>
-                To get started, edit <code>src/App.tsx</code> and save to reload.
-            </p>
-        </section>
-    );
+	const onChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { files } = event.target;
+
+		// FileReader support
+		if (FileReader && files && files.length) {
+			const fr = new FileReader();
+			fr.onload = () => {
+				if (typeof fr.result === 'string') {
+					setImageUrl(fr.result);
+				}
+			};
+			fr.readAsDataURL(files[0]);
+		}
+
+		// Not supported
+		else {
+			// fallback -- perhaps submit the input to an iframe and temporarily store
+			// them on the server until the user's session ends.
+		}
+
+	};
+
+	return (
+		<section className={home()}>
+			<header className={home('Header')} onClick={onHeaderClick}>
+				<img src={logo} className={home('Logo')} alt="logo" />
+				<h1 className={home('Title')}>{text || 'Welcome to React'}</h1>
+				<input type="file" onChange={onChangeFile} />
+			</header>
+			<p className={home('Intro')}>
+				{imageUrl ? (
+					<img src={imageUrl} alt="" />
+				) : null}
+			</p>
+		</section>
+	);
 };
 
 export default Home;
 
 interface IProps {
-    text?: string;
-    onHeaderClick?: () => void;
+	text?: string;
+	onHeaderClick?: () => void;
 }
--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,52 +1,74 @@
-import { BrowserWindow, app, ipcMain } from 'electron';
+import { BrowserWindow, app, ipcMain, globalShortcut } from 'electron';
 import { handleSquirrelEvent } from './helpers';
 import * as isDev from 'electron-is-dev';
 
 if (!handleSquirrelEvent(app)) {
-    let mainWindow: Electron.BrowserWindow | null = null;
+	let mainWindow: Electron.BrowserWindow | null = null;
 
-    const createWindow = async () => {
-        mainWindow = new BrowserWindow({
-            width: 800,
-            height: 800
-        });
+	const createWindow = async () => {
+		mainWindow = new BrowserWindow({
+			width: 800,
+			height: 400,
+			transparent: true,
+			frame: false,
+			webPreferences: {
+				// devTools: false
+			}
+		});
 
-        mainWindow.setMinimizable(false);
-        mainWindow.setMaximizable(false);
-        mainWindow.loadURL(`file://${__dirname}/index.html`);
-        mainWindow.webContents.openDevTools();
+		mainWindow.setMinimizable(false);
+		mainWindow.setMaximizable(false);
+		mainWindow.loadURL(`file://${__dirname}/index.html`);
+		// mainWindow.webContents.openDevTools();
+		mainWindow.webContents.openDevTools({ mode: 'undocked' });
 
-        mainWindow.on('closed', () => { mainWindow = null; });
+		app.commandLine.appendSwitch('enable-transparent-visuals');
 
-        // Send messages
-        mainWindow.webContents.send('cl', '123466');
+		mainWindow.on('closed', () => { mainWindow = null; });
 
-        // Listen messages
-        ipcMain.on('message', () => console.log(1));
+		// Send messages
+		mainWindow.webContents.send('cl', '123466');
 
-        mainWindow.webContents.on('did-finish-load', () => {
-            if (mainWindow) {
-                if (!isDev) {
-                    // run auto updater
-                }
-            }
-        });
+		app.on('browser-window-focus', () => {
+			globalShortcut.register('CommandOrControl+R', () => {
+				console.log('CommandOrControl+R is pressed: Shortcut Disabled');
+			});
+			globalShortcut.register('F5', () => {
+				console.log('F5 is pressed: Shortcut Disabled');
+			});
+		});
 
-        process.on('uncaughtException', (error) => mainWindow && mainWindow.webContents.send('cl', error));
-    };
+		app.on('browser-window-blur', () => {
+			globalShortcut.unregister('CommandOrControl+R');
+			globalShortcut.unregister('F5');
+		});
 
-    app.on('ready', createWindow);
+		// Listen messages
+		ipcMain.on('message', () => console.log(1));
 
-    // Quit when all windows are closed.
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
+		mainWindow.webContents.on('did-finish-load', () => {
+			if (mainWindow) {
+				if (!isDev) {
+					// run auto updater
+				}
+			}
+		});
 
-    app.on('activate', () => {
-        if (mainWindow === null) {
-            createWindow();
-        }
-    });
+		process.on('uncaughtException', (error) => mainWindow && mainWindow.webContents.send('cl', error));
+	};
+
+	app.on('ready', createWindow);
+
+	// Quit when all windows are closed.
+	app.on('window-all-closed', () => {
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	});
+
+	app.on('activate', () => {
+		if (mainWindow === null) {
+			createWindow();
+		}
+	});
 }
